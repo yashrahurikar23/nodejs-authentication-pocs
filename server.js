58247e1073188f8b0bd4417bde7f7de4a4b3bc89
Yash Rahurikar <2yashrahurikar@gmail.com>
2021-03-11 05:02:37 +0530

    1. Installed jsonwebtoken package 2. signing and returning jwt tokens on user login 3. authenticate token helper for authenticating the token for users list API

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
+const { authenticateToken } = require('./helpers/auth')
 
 const app = express();
  
@@ -12,7 +15,7 @@ app.use(bodyParser.json())
 
 const users = [];
 
-app.get('/users', async (req, res) => {
+app.get('/users', authenticateToken, async (req, res) => {
   res.status(200).send(users);
 })
 
@@ -36,7 +39,8 @@ app.post('/users/login', async (req, res) => {
 
   try {
     if(await bcrypt.compare(req.body.password, user.password)) {
-      res.status(200).send('User is authenticated!');
+      const accessToken = jwt.sign(user, process.env.SECRET_ACCESS_TOKEN);
+      res.status(200).send({ accessToken: accessToken });
     } else {
       res.status(400).send('User is not autheticated!');
     }
